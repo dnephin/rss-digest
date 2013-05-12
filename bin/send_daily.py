e90e084f6d75f7f118f698e80e6a67e7f25c5bb6
Daniel Nephin <dnephin@gmail.com>
2013-05-12 11:13:01 -0700

    Add api key flag to send daily.

--- bin/send_daily.py
+++ bin/send_daily.py
@@ -12,12 +12,13 @@ def parse_opts():
     parser.add_option('-c', '--email-config', default='config/email.yaml')
     parser.add_option('--rssdigest-config', default=config.path)
     parser.add_option('-o', '--override', action='append', default=[])
+    parser.add_option('--api-key')
     opts, args = parser.parse_args()
     return opts
 
 
 def load_config(opts):
-    config.load_app_config()
+    config.load_app_config(opts.api_key)
     staticconf.YamlConfiguration(opts.email_config)
     config.load(opts.rssdigest_config)
     staticconf.ListConfiguration(opts.override)
